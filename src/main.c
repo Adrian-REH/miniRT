94740f3773f3b125036ec8ced117c7808c44637b
Adrian-REH <adrianherrera.r.e@gmail.com>
2024-11-03 16:16:45 +0100

    Update rays

--- src/main.c
+++ src/main.c
@@ -15,8 +15,12 @@
 #include <math.h>
 #define WINX 1280
 #define WINY 720
+# define X 0
+# define Y 1
 #include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 
 
 typedef struct {
@@ -37,7 +41,62 @@ typedef struct {
     Vector3 normal;
     Vector3 point;
 } Plane;
+int	ft_round(double num)
+{
+	int	rounded;
 
+	rounded = (int)num;
+	if (num - rounded >= .5)
+		rounded++;
+	return (rounded);
+}
+int	gradient(int startcolor, int endcolor, int len, int pix)
+{
+	double	increment[3];
+	int		new[3];
+	int		newcolor;
+
+	increment[0] = (double)((endcolor >> 16) - \
+					(startcolor >> 16)) / (double)len;
+	increment[1] = (double)(((endcolor >> 8) & 0xFF) - \
+					((startcolor >> 8) & 0xFF)) / (double)len;
+	increment[2] = (double)((endcolor & 0xFF) - (startcolor & 0xFF)) \
+					/ (double)len;
+	new[0] = (startcolor >> 16) + ft_round(pix * increment[0]);
+	new[1] = ((startcolor >> 8) & 0xFF) + ft_round(pix * increment[1]);
+	new[2] = (startcolor & 0xFF) + ft_round(pix * increment[2]);
+	newcolor = (new[0] << 16) + (new[1] << 8) + new[2];
+	return (newcolor);
+}
+int	draw_line(Vector3 start, Vector3 end, void *win, void *mlx)
+{
+	Vector3	delta;
+	Vector3	pixel;
+	int		pixels;
+	int		len;
+    int color;
+    int init_color;
+    int end_color;
+
+	delta.x = end.x - start.x;
+	delta.y = end.y - start.y;
+	delta.z = end.z - start.z;
+	pixels = sqrt((delta.x * delta.x) + (delta.y * delta.y) + (delta.z * delta.z));
+	len = pixels;
+	delta.x /= pixels;
+	delta.y /= pixels;
+	pixel.x = start.x;
+	pixel.y = start.y;
+	while (pixels > 0)
+	{
+		//color = gradient(init_color, end_color, len, len - pixels);
+        mlx_pixel_put(mlx, win, pixel.x, pixel.y, 0xFFFFFF);
+		pixel.x += delta.x;
+		pixel.y += delta.y;
+		pixels = pixels - 1;
+	}
+	return (1);
+}
 void	set_color(char *buffer, int endian, int color, int alpha)
 {
 	if (endian == 1)
@@ -166,7 +225,17 @@ int adjust_color(int base_color, double intensity) {
     // Combinamos los componentes ajustados en un solo color hexadecimal
     return (red << 16) | (green << 8) | blue;
 }
-
+void normalize(Vector3 *v) {
+    // Calcula la magnitud del vector
+    double magnitude = sqrt(v->x * v->x + v->y * v->y + v->z * v->z);
+    
+    // Evita la división por cero
+    if (magnitude > 0) {
+        v->x /= magnitude;
+        v->y /= magnitude;
+        v->z /= magnitude;
+    }
+}
 int adjust_gradient_color(int base_color, int background_color, double intensity, double alpha) {
     // Extraemos los componentes RGB del color base
     int base_red = (base_color >> 16) & 0xFF;
@@ -271,16 +340,13 @@ Vector3 *camera_dir(Vector3 camera_pos, Vector3 hit_pt){
 	return view_dir;
 }
 
-
 double calculate_intensity(Vector3 normal, Vector3 light_dir) {
     return fmax(0.0, normal.x * light_dir.x + normal.y * light_dir.y + normal.z * light_dir.z);
 }
 
-
-
-
-void render_sphere(void *mlx, void *win, int screen_width, int screen_height, Sphere sphere, Plane plane,Plane plane2,Plane plane3, Vector3 camera_pos) {
-    Vector3 light = {0, -1, 10}; // luz
+void render_sphere(void *mlx, void *win, int screen_width, int screen_height, Sphere sphere, Plane *plans, Vector3 camera_pos) {
+    Vector3 light = {10, 10, -10}; // luz
+   // normalize(&light);
     time_t start, end;
 	int color = 0x33cc55;
     start = clock();
@@ -293,31 +359,45 @@ void render_sphere(void *mlx, void *win, int screen_width, int screen_height, Sp
             Ray ray = *generate_ray(x, y, WINX, WINY, camera_pos);
            	Ray rayslight = *generate_ray(x, y, WINX, WINY, light);
             double t = 0;
-			printf("(%f, %f, %f)\n", ray.direction.x,ray.direction.y,ray.direction.z);
- 			 if ( (x > (screen_width / 2))&& intersect_plane(&ray, &plane3, &t))
+	 		 if (intersect_plane(&ray, &plans[0], &t))
 			{
-				Vector3 *hit_pt = hit_point(rayslight, t);
-				Vector3 *l_dir = light_dir(plane3.normal, light, *hit_pt);
-				double intensity = calculate_intensity(plane3.normal, *l_dir);
-				int current_color = mix_colors(0xD89A0B, 0, intensity);
-				hit_pt = hit_point(ray, t);
+				Vector3 *hit_pt = hit_point(ray, t);
 				Vector3 *cam_dir = camera_dir(camera_pos, *hit_pt);
-				intensity = calculate_intensity(plane3.normal, *cam_dir);
+				double intensity = calculate_intensity(plans[0].normal, *cam_dir);
+				int current_color = mix_colors(0xD89A0B, 0xD89A0B, intensity);
+                intersect_plane(&rayslight, &plans[0], &t);
+				hit_pt = hit_point(rayslight, t);
+				Vector3 *l_dir = light_dir(plans[0].normal, light, *hit_pt);
+				intensity = calculate_intensity(plans[0].normal, *l_dir);
 				int new_color = mix_colors(0x33cc55, current_color, intensity);
 				mlx_pixel_put(mlx, win, x, y, new_color);
 			}
-	 		 if ((x < (screen_width / 2))&& intersect_plane(&ray, &plane2, &t))
+  /* 			 if ( (x > (screen_width / 2)) && intersect_plane(&ray, &plans[2], &t))
 			{
-				Vector3 *hit_pt = hit_point(rayslight, t);
-				Vector3 *l_dir = light_dir(plane2.normal, light, *hit_pt);
-				double intensity = calculate_intensity(plane2.normal, *l_dir);
-				int current_color = mix_colors(0xD89A0B, 0, intensity);
-				hit_pt = hit_point(ray, t);
+				Vector3 *hit_pt = hit_point(ray, t);
 				Vector3 *cam_dir = camera_dir(camera_pos, *hit_pt);
-				intensity = calculate_intensity(plane2.normal, *cam_dir);
-				int new_color = mix_colors(0xF05A7E, current_color, intensity);
+				double intensity = calculate_intensity(plans[2].normal, *cam_dir);
+				int current_color = mix_colors(0x19b191, 0x19b191, intensity);
+                intersect_plane(&rayslight, &plans[2], &t);
+				hit_pt = hit_point(rayslight, t);
+				Vector3 *l_dir = light_dir(plans[2].normal, light, *hit_pt);
+				intensity = calculate_intensity(plans[2].normal, *l_dir);
+				int new_color = mix_colors(0x33cc55, current_color, intensity);
 				mlx_pixel_put(mlx, win, x, y, new_color);
 			}
+	 		else if ((x < (screen_width / 2)) && intersect_plane(&ray, &plans[1], &t))
+			{
+				Vector3 *hit_pt = hit_point(ray, t);
+				Vector3 *cam_dir = camera_dir(camera_pos, *hit_pt);
+				double intensity = calculate_intensity(plans[1].normal, *cam_dir);
+				int current_color = mix_colors(0xF05A7E, 0xF05A7E, intensity);
+                intersect_plane(&rayslight, &plans[1], &t);
+				hit_pt = hit_point(rayslight, t);
+				Vector3 *l_dir = light_dir(plans[1].normal, light, *hit_pt);
+				intensity = calculate_intensity(plans[1].normal, *l_dir);
+				int new_color = mix_colors(0x33cc55, current_color, intensity);
+				mlx_pixel_put(mlx, win, x, y, new_color);
+			} */
 			if (intersect_sphere(&ray, &sphere, &t))
 			{
 				Vector3 *hit_pt = hit_point(ray, t);
@@ -326,14 +406,18 @@ void render_sphere(void *mlx, void *win, int screen_width, int screen_height, Sp
 				double intensity = calculate_intensity(*n_sphere, *cam_dir);
 				int current_color = mix_colors(0x1864AA, 0, intensity);
 				Vector3 *l_dir = light_dir(*n_sphere, light, *hit_pt);
+                intersect_sphere(&rayslight, &sphere, &t);
 				hit_pt = hit_point(rayslight, t);
 				n_sphere = normal_sphere(*hit_pt, sphere);
 				intensity = calculate_intensity(*n_sphere, *l_dir);
 				int new_color = mix_colors(0x33cc55, current_color, intensity);
 				mlx_pixel_put(mlx, win, x, y, new_color);
-			} 
+               // draw_line(camera_pos, (Vector3){x, y, 10}, win, mlx);
+			}
+          //  draw_line(ray.origin,ray.direction, win, mlx);
 		}
 	}
+    
 	end = clock();
 	double elapsed = (double)(end - start) / CLOCKS_PER_SEC * 1000; // Convertido a milisegundos
 	printf("Elapsed time: %.3f milliseconds\n", elapsed);
@@ -360,14 +444,18 @@ int main()
 	buffer = mlx_get_data_addr(img, &bitxpixel, &lines, &endian);
     Vector3 camera_pos = {0, 0, 0};      // Cámara en el origen
     Sphere sphere = {{0, 0, 5}, 1};      // Esfera en Z = 5 con radio 1
-    Plane plane = {{0, 1, 0}, {0, 0, -10}};  //
-    Plane plane2 = {{1, 1, 0}, {-2, 0, 0}};  // 
-    Plane plane3 = {{-1, 1, 0}, {2, 0, 0}};  //
+    Plane *plans = malloc(sizeof(Plane) * 5);
+    plans[0] =  (Plane){{1, 0, 0}, {0, 0, 0}};
+    plans[1] =  (Plane){{1, 1, 0}, {-2, 0, 0}};
+    plans[2] =  (Plane){{-1, 1, 0}, {2, 0, 0}};
+    plans[3] =  (Plane){{0, 0, 1}, {0, 0, 0}};
+    plans[4] =  (Plane){{0, 1, 0}, {0, 0, 0}};
+    normalize(&plans[0].normal);
+    normalize(&plans[1].normal);
+    normalize(&plans[2].normal);
+    normalize(&plans[3].normal);
+    normalize(&plans[4].normal);
+    render_sphere(mlx, win, WINX, WINY, sphere, plans, camera_pos);
 
-
-    render_sphere(mlx, win, WINX, WINY, sphere, plane, plane2, plane3, camera_pos);
-
-	//peint_plane(buffer, endian, lines,  color);
-	//mlx_put_image_to_window(mlx, win, 0, 1);
 	mlx_loop(mlx);
 }
