c919ae1a17c0dc9e5b1c9dfd9361f6d11e1256fe
Randolph David Zamora Urbaez <razamora@c1r12s2.42madrid.com>
2024-11-21 21:09:42 +0100

    libbrdf libmapfun libmath

--- src/lib/libbrdf/reflect.c
+++ src/lib/libbrdf/reflect.c
@@ -1,14 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reflect.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: razamora <razamora@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 22:11:37 by razamora          #+#    #+#             */
+/*   Updated: 2024/11/21 21:04:16 by razamora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../../main.h"
-Vector3 *reflect(Vector3 L, Vector3 N)
+
+Vector3	*reflect(Vector3 L, Vector3 N)
 {
-	double dot_product = dot(N, L); // N dot L
-	Vector3 *reflection = malloc(sizeof(Vector3));
-	*reflection =  (Vector3) {
+	Vector3	*reflection;
+	double	dot_product;
+
+	dot_product = dot(N, L); // N dot L
+	reflection = malloc(sizeof(Vector3));
+	*reflection = (Vector3){
 		2 * dot_product * N.x - L.x,
 		2 * dot_product * N.y - L.y,
-		2 * dot_product * N.z - L.z
-	};
-	 normalize(reflection); // Normalize the reflected vector
-	return reflection;
+		2 * dot_product * N.z - L.z};
+	normalize(reflection); // Normalize the reflected vector
+	return (reflection);
 }
--- src/lib/libmath/solve_quadratic.c
+++ src/lib/libmath/solve_quadratic.c
@@ -1,19 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   solve_quadratic.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: razamora <razamora@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/21 21:06:54 by razamora          #+#    #+#             */
+/*   Updated: 2024/11/21 21:08:34 by razamora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../main.h"
 
-int solve_quadratic(double a, double b, double c, double* t0, double* t1)
+int	solve_quadratic(double a, double b, double c, double* t0, double* t1)
 {
 	double	discriminant;
-	double q;
+	double	q;
+	double	temp;
+
 	discriminant = b * b - 4 * a * c;
 	if (discriminant < 0)
 		return (0);
 
-	q = (b > 0) ? -0.5 * (b + sqrt(discriminant)) : -0.5 * (b - sqrt(discriminant));
+	q = (b > 0) ? -0.5 * (b + sqrt(discriminant)) : -0.5 * \
+	(b - sqrt(discriminant));
 	*t0 = q / a;
 	*t1 = c / q;
 	if (*t0 > *t1)
 	{
-		double temp = *t0;
+		temp = *t0;
 		*t0 = *t1;
 		*t1 = temp;
 	}
